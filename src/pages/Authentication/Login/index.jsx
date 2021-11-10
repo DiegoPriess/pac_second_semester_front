fe5b798747701178ca65c032f3e3c2b97b7f7e4a
Diego Priess <diegopriess.dev@gmail.com>
2021-11-10 13:19:37 -0300

    Alterando tela de cadastro de usuário e adicionando input para foto de perfil

--- src/pages/Authentication/Login/index.jsx
+++ src/pages/Authentication/Login/index.jsx
@@ -9,9 +9,11 @@ const Login = () => {
         <form action="">
             <InputGroup icon="account_circle" labelText="Usuário ou E-mail" inputType="text"/>
             <InputGroup icon="lock" id="password" labelText="Senha" inputType="password"/>
-            <Link to={"/menu"}><Button textButton="Entrar"/></Link>
-            <span className="button-space">Ou</span>
-            <Link to={"/register"}><Button customClass="register-button" textButton="Cadastrar-se"/></Link>
+            <div className="actions-container">
+                <Link to={"/menu"}><Button textButton="Entrar"/></Link>
+                <span className="button-space">Ou</span>
+                <Link to={"/register"}><Button customClass="register-button" textButton="Cadastrar-se"/></Link>
+            </div>
         </form>    
     );
 }
