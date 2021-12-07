b3124426e1098bdcba01391b11a0c348a22466f6
Diego Priess <diegopriess.dev@gmail.com>
2021-12-06 23:18:20 -0300

    Removendo get personalizado

--- src/api/api.js
+++ src/api/api.js
@@ -3,8 +3,3 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'http://localhost:3000/'
 })
-
-export const get = async(url, setData) => { 
-  const reposta = await api.get(url);
-  setData(reposta.data);
-}
