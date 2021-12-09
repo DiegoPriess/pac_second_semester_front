294c62b8eb028feff92ec13b8747f2e7fb47d633
Diego Priess <diegopriess.dev@gmail.com>
2021-12-08 23:00:39 -0300

    removendo import

--- src/components/ProfileChangePassword/index.jsx
+++ src/components/ProfileChangePassword/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 import ReactDOM from 'react-dom';
 import { api } from '../../api/api';
